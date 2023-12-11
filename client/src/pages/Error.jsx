3511ec101cd59f13d667a010b7f12137ebd968e5
Arvind Kumar <thakurarvindkr10@gmail.com>
2023-12-11 19:02:03 +0530

    added custom error

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  AiOutlineHome,
+  AiOutlineLeft,
+  AiOutlineArrowLeft,
+} from "react-icons/ai";
+import { Link, useNavigate, useRouteError } from "react-router-dom";
 
 const Error = () => {
+  const error = useRouteError();
+  const navigate = useNavigate();
   return (
-    <div>Error</div>
-  )
-}
+    <div className="error">
+      <h1>Uh oh! We've got a problem.</h1>
+      <p>{error.message || error.statusText}</p>
+      <div className="flex-md">
+        <button onClick={() => navigate(-1)} className="btn btn--dark">
+          <AiOutlineArrowLeft />
+          <span>Go Back</span>
+        </button>
+        <Link to={"/"} className="btn btn--dark">
+          <AiOutlineHome width={20} />
+          <span>Go Home</span>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Error
+export default Error;
